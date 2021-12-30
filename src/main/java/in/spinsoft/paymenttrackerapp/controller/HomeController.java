9126b7c15b8b76e17ce2b20feb19389403f8d05b
Rajesh <vbtperk.rajesh@gmail.com>
2021-12-30 15:58:48 +0530

    Fixed port no

--- src/main/java/in/spinsoft/paymenttrackerapp/controller/HomeController.java
+++ src/main/java/in/spinsoft/paymenttrackerapp/controller/HomeController.java
@@ -1,15 +1,19 @@
 package in.spinsoft.paymenttrackerapp.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
 @RequestMapping
 public class HomeController {
 
 	@GetMapping()
 	public String home() {
+		log.debug("HomeController -> home");
 		return "index";
 	}
 }
