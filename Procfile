f275908fb4a523e7e89d1851639e420d917f9390
Rajesh <vbtperk.rajesh@gmail.com>
2021-12-30 13:54:39 +0530

    Initial Project Setup

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java -jar target/paymenttracker-api-1.0.jar
---
+++ src/test/java/in/spinsoft/paymenttrackerapp/ApplicationTests.java
@@ -0,0 +1,13 @@
+package in.spinsoft.paymenttrackerapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
