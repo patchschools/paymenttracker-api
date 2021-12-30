2a0758a7e2d22cf02db8c7be44cfae87fa4956f7
Rajesh <vbtperk.rajesh@gmail.com>
2021-12-30 15:02:29 +0530

    Added swagger configuration

--- src/main/java/in/spinsoft/paymenttrackerapp/Application.java
+++ src/main/java/in/spinsoft/paymenttrackerapp/Application.java
@@ -3,12 +3,19 @@ package in.spinsoft.paymenttrackerapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+	
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
 	}
 
 }
